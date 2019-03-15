5301f20ca27a845588ce57058ca477b24f9e17b9
Kuni <kuni.ujiie@grapecity.com>
2019-03-15 09:33:23 +0900

    Updated.

--- WebAPIApp/Models/grapecitytestdbContext.cs
+++ WebAPIApp/Models/grapecitytestdbContext.cs
@@ -33,7 +33,7 @@ namespace WebAPIApp.Models
 //            if (!optionsBuilder.IsConfigured)
 //            {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-//                optionsBuilder.UseSqlServer("Server=tcp:grapecitytestdbsrv.database.windows.net,1433;Initial Catalog=grapecitytestdb;Persist Security Info=False;User ID=kuni;Password=Leadtoolsv19;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+//                optionsBuilder.UseSqlServer("");
 //            }
 //        }
 
